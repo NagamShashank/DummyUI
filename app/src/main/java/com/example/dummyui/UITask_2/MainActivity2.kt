0e94114a446395bd35bd983a0acb0bac6eb1c505
Shashank Nagam <shashanknagam@nimapinfotech.com>
2024-02-03 10:24:11 +0530

    Commit 1

--- app/src/main/java/com/example/dummyui/UITask_2/MainActivity2.kt
+++ app/src/main/java/com/example/dummyui/UITask_2/MainActivity2.kt
@@ -2,6 +2,8 @@ package com.example.dummyui.UITask_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import com.example.dummyui.R
 import com.example.dummyui.databinding.ActivityMain2Binding
@@ -14,9 +16,9 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-
+//        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         replaceFragment(Fragment_Home())
-
         binding.bottomNavigation.setOnItemSelectedListener {
             it.isChecked = true
 
